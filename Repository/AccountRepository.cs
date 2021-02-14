255297211f535dc6cc86a37626b58ad7c8ba3c66
Robel Dereje <wakgarirobel@gmail.com>
2021-02-14 21:48:10 +0300

    Add project files.

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class AccountRepository: RepositoryBase<Account>,IAccountRepository
+    {
+        public AccountRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+        }
+    }
+}
